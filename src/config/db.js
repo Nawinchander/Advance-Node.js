1c71f7157bd608a9302124021afff7d9abba0f17
Nawin chander <nawinnapzy289@gmail.com>
2026-03-23 11:58:35 +0530

    add the changes

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ const { DB_URI } = require("./env");
 const connectDB = async () => {
   try {
     await mongoose.connect(DB_URI);
-    console.log("✅ DB Connected");
+    console.log(" DB Connected");
   } catch (err) {
-    console.error("❌ DB Error:", err);
+    console.error(" DB Error:", err);
     process.exit(1);
   }
 };
